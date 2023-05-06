b8cb10a17173c0026b9f3148d18fcf27b0548d5d
haichen cheng <haichen_cheng@hotmail.com>
2023-05-06 12:40:32 -0700

    Add test run forward 3 seconds

--- test/core_test/arena_test.py
+++ test/core_test/arena_test.py
@@ -7,66 +7,88 @@ from src.core.track import *
 from arena_sample import ArenaSample
 
 class ArenaTest(unittest.TestCase):
+    def setUp(self):
+        print('\n===\nArenaTest.setUp()')
     
-    def test_100_arena(self):
-        print('\n===\ntest_100_arena')
-
-        arena = ArenaSample.sample_arena_0()
-        print(arena)
-        print('view_radius =', arena.view_radius)
-        print('car_config =', arena.car_config)
-        print('track_field =\n', arena.track_field.field)
-        print('field.shape =', arena.track_field.field.shape)
+        self.arena = ArenaSample.sample_arena_0()
+        print(self.arena)
+        print('view_radius =', self.arena.view_radius)
+        print('car_config =', self.arena.car_config)
+        print('track_field =\n', self.arena.track_field.field)
+        print('field.shape =', self.arena.track_field.field.shape)
 
 
-        start_state = car.CarState(position = car.Point2D(y = 5.5, x = 14.5))
-        print('start_state = ', start_state)
-        self.assertTrue(start_state.trackDistance == 0)
-        self.assertTrue(start_state.velocity_x == 0)
-        self.assertTrue(start_state.velocity_y == 0)
-        self.assertTrue(start_state.wheel_angle == 0)
-        
-        start_view = arena.get_car_view(start_state)
+        self.start_state = car.CarState(position = car.Point2D(y = 5.5, x = 14.5))
+        print('start_state = ', self.start_state)
+        self.assertTrue(self.start_state.trackDistance == 0)
+        self.assertTrue(self.start_state.velocity_x == 0)
+        self.assertTrue(self.start_state.velocity_y == 0)
+        self.assertTrue(self.start_state.wheel_angle == 0)
+
+        self.time_interval = 100 # 100 msec
+
+
+    def test_100_startview(self):
+        print('\n===\ntest_100_startview()')
+
+        print('car_config = ', self.arena.car_config)
+        print('time_interval = ', self.time_interval)
+
+        start_view = self.arena.get_car_view(self.start_state)
         print('start_view = ', start_view)
 
-        print('car_config = ', arena.car_config)
-    
-        time_interval = 100 # 100 msec
 
-        """
+    def test_200_too_low_power(self):
+        print('\n===\ntest_200_too_low_power()')
+    
         low_power_action = car.Action(1,0)
         print('low_power_action = ', low_power_action)
-        state_1 = arena.get_next_state(start_state, low_power_action, time_interval)
+        state_1 = self.arena.get_next_state(self.start_state, low_power_action, self.time_interval)
         print('state_1 = ', state_1)
         self.assertTrue(state_1.velocity_x == 0)
         self.assertTrue(state_1.velocity_y == 0)
         self.assertTrue(state_1.wheel_angle == 0)
-        self.assertTrue(state_1.trackDistance=0 == 0)
-        self.assertTrue(state_1.timestamp == time_interval)
-        """
+        self.assertTrue(state_1.trackDistance == 0)
+        self.assertTrue(state_1.timestamp == self.time_interval)
 
+
+    def test_300_startable_power(self):
+        print('\n===\ntest_300_startable_power()')
 
         startable_power_action = car.Action(2,0)
         print('startable_power_action = ', startable_power_action)
-        state_2 = arena.get_next_state(start_state, startable_power_action, time_interval)
+        state_2 = self.arena.get_next_state(self.start_state, startable_power_action, self.time_interval)
         print('state_2 = ', state_2)
         self.assertTrue(abs(state_2.velocity_x - 0.15) < 1e-5)
         self.assertTrue(state_2.velocity_y == 0)
         self.assertTrue(state_2.wheel_angle == 0)
         self.assertTrue(state_2.trackDistance == 0)
-        self.assertTrue(state_2.timestamp == time_interval)
-        self.assertTrue(state_2.position.x == start_state.position.x)
-        self.assertTrue(state_2.position.y == start_state.position.y)
+        self.assertTrue(state_2.timestamp == self.time_interval)
+        self.assertTrue(state_2.position.x == self.start_state.position.x)
+        self.assertTrue(state_2.position.y == self.start_state.position.y)
 
-        state_3 = arena.get_next_state(state_2, startable_power_action, time_interval)
+        state_3 = self.arena.get_next_state(state_2, startable_power_action, self.time_interval)
         print('state_3 = ', state_3)
         self.assertTrue(abs(state_3.velocity_x - 0.3) < 1e-5)
         self.assertTrue(state_3.velocity_y == 0)
         self.assertTrue(state_3.wheel_angle == 0)
         self.assertTrue(state_3.trackDistance == 0)
-        self.assertTrue(state_3.timestamp - state_2.timestamp == time_interval)
-        self.assertTrue(abs(state_3.position.x - start_state.position.x - 0.015) < 1e-5)
-        self.assertTrue(state_3.position.y == start_state.position.y)
+        self.assertTrue(state_3.timestamp - state_2.timestamp == self.time_interval)
+        self.assertTrue(abs(state_3.position.x - self.start_state.position.x - 0.015) < 1e-5)
+        self.assertTrue(state_3.position.y == self.start_state.position.y)
+
+
+    def test_400_startable_fix_power(self):
+        print('\n===\ntest_300_startable_power()')
+
+        startable_power_action = car.Action(2,0)
+
+        current_state = self.start_state
+        print(current_state)
+        while current_state.timestamp < 3000:
+            current_state = self.arena.get_next_state(current_state, startable_power_action, self.time_interval)
+            print(current_state)
+
 
 if __name__ == '__main__':
     unittest.main()
