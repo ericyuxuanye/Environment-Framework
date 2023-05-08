9d97b065b55713b8c13cc48cf947c212eecd6970
haichen cheng <haichen_cheng@hotmail.com>
2023-05-08 07:33:27 -0700

    Setup race test

--- src/core/race.py
+++ src/core/race.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-import datetime
+from datetime import datetime
 
 from src.core import model, car, track
 
@@ -13,7 +13,7 @@ class ModelInfo:
 
 @dataclass 
 class ArenaInfo:
-    __slots__ = "track_name", "setup_id"
+    __slots__ = "track_name", "view_radius", "car_config"
 
     track_name: str
     view_radius: int
@@ -31,21 +31,27 @@ class CarStateAction:
 
 @dataclass 
 class RaceConfig:
-    __slots__ = "model_info", "start_time"
+    __slots__ = "arena_info", "round_to_finish", "model_info", "car_info"
 
     arena_info: ArenaInfo
     round_to_finish: int
     model_info: ModelInfo
+    car_info: car.CarInfo
 
-    def __init__(self, arena_info:ArenaInfo, round_to_finish:int, model_info:ModelInfo):
+    def __init__(self, 
+                 arena_info: ArenaInfo, 
+                 round_to_finish: int, 
+                 model_info: ModelInfo, 
+                 car_info: car.CarInfo):
         self.arena_info = arena_info
         self.round_to_finish = round_to_finish
         self.model_info = model_info
+        self.car_info = car_info
 
 
 @dataclass 
 class RaceDataset:
-    __slots__ = "model_info", "start_time"
+    __slots__ = "race_config", "start_time", "steps"
 
     race_config: RaceConfig
     start_time: datetime
@@ -53,20 +59,23 @@ class RaceDataset:
 
     def __init__(self, race_config:RaceConfig):
         self.race_config = race_config
+        self.steps = []
 
-    
 
 @dataclass 
 class Race:
-    __slots__ = "model_info", "start_time"
+    __slots__ = "data", "model"
 
     data : RaceDataset
+    model : model.IModelInference 
 
-    def __init__(self, race_config:RaceConfig):
+    def __init__(self, race_config:RaceConfig, model: model.IModelInference):
         self.data = RaceDataset(race_config)
+        self.model = model
 
-    def run():
+    def run(self):
         print('start')
+        self.data.start_time = datetime.now()
 
         print('finish')
 
--- test/core_test/race_test.py
+++ test/core_test/race_test.py
@@ -5,15 +5,16 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 import unittest
 import math
 from src.core import race
+from race_sample import RaceSample
 
 class RaceTest(unittest.TestCase):
 
     def test_000_simple(self):
         print('\n===\ntest_000_simple')
+        race = RaceSample.sample_race_0()
 
-
-
-
+        race.run()
+        print('race data\n', race.data)
 
 if __name__ == '__main__':
     unittest.main()
